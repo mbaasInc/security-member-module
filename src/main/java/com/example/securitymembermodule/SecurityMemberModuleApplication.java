a617296d46594a7391af3fe453c70f87877f237e
KangHuiSoo <org.woodie@gmail.com>
2023-08-10 22:11:18 +0900

    Initial commit

---
+++ src/main/java/com/example/securitymembermodule/SecurityMemberModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitymembermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMemberModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityMemberModuleApplication.class, args);
+    }
+
+}
